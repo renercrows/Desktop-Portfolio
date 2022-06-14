a0378c2a30507631252beed7b3a40a8ad2f5f574
renercrows <ungredda95@gmail.com>
2022-06-14 14:09:14 -0400

    Fix CSS and JavaScript

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const img = document.querySelector(".union0");
-const menu = document.querySelector(".second-menu");
-const li = document.querySelectorAll(".second-menu li")
+const img = document.querySelector('.union0');
+const menu = document.querySelector('.second-menu');
+const li = document.querySelectorAll('.second-menu li');
 
-let listItem = Array.from(li);
+const listItem = Array.from(li);
 
-let menuTgl = ()=> {
-    menu.classList.toggle("visibility"); }
+const menuTgl = () => {
+  menu.classList.toggle('visibility');
+};
 
 img.addEventListener("click", menuTgl);
 
-listItem.forEach(child => {
-    child.addEventListener("click", menuTgl);
+listItem.forEach((child) => {
+  child.addEventListener('click', menuTgl);
 });
 
-document.getElementById('clickme').onclick=function() {
-    this.classList.toggle('active');
-   }
+document.getElementById('clickme').onclick = function (e) {
+  this.classList.toggle('active');
+};
