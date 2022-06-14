0dcf44006f6eb875f96d294a083682b42043b6cb
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-06-14 15:39:52 -0400

    add script.js file for menu-mobile

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const img = document.querySelector('.union0');
+const menu = document.querySelector('.second-menu');
+const li = document.querySelectorAll('.second-menu li');
+const listItem = Array.from(li);
+const menuTgl = () => {
+  menu.classList.toggle('visibility');
+};
+img.addEventListener('click', menuTgl);
+listItem.forEach((child) => {
+  child.addEventListener('click', menuTgl);
+});
+document.getElementById('clickme').onclick = function click() {
+  this.classList.toggle('active');
+};
