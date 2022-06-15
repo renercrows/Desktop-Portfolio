3c22a88aa5333a56bf27034be5ec7c8467a5b5ae
renercrows <ungredda95@gmail.com>
2022-06-15 11:46:11 -0400

    Fix JavaScript Code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const hamburguer = document.querySelector('.hamburguer');
 const navMenu = document.querySelector('.nav-menu');
 const navLogo = document.querySelector('.nav-logo');
@@ -18,19 +17,3 @@ document.querySelectorAll('.hamburguer').forEach(n => n.
 addEventListener('click', () => {
     navLogo.classList.toggle('active');
 }))
-=======
-const img = document.querySelector('.union0');
-const menu = document.querySelector('.second-menu');
-const li = document.querySelectorAll('.second-menu li');
-const listItem = Array.from(li);
-const menuTgl = () => {
-  menu.classList.toggle('visibility');
-};
-img.addEventListener('click', menuTgl);
-listItem.forEach((child) => {
-  child.addEventListener('click', menuTgl);
-});
-document.getElementById('clickme').onclick = function click() {
-  this.classList.toggle('active');
-};
->>>>>>> 0dcf44006f6eb875f96d294a083682b42043b6cb
