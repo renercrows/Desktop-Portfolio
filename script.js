4098f3199a921e73028a36c949d1774188a4ab7b
renercrows <ungredda95@gmail.com>
2022-06-22 22:58:16 -0400

    Fix CSS and JavaScript Code

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const navLogo = document.querySelector('.nav-logo');
 hamburguer.addEventListener('click', () => {
   hamburguer.classList.toggle('active');
   navMenu.classList.toggle('active');
+  navLogo.classList.toggle('active')
 });
 
-document.querySelectorAll('.hamburguer').forEach((n) => n.addEventListener('click', () => {
-  navLogo.classList.toggle('active');
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburguer.classList.remove('active');
+  navMenu.classList.remove('active');
+  navLogo.classList.toggle('active')
 }));
